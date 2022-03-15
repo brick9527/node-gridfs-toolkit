3ff1c8ce77aefdb410cada84b86bb790d9c9bbc6
brick9527 <brick9527@foxmail.com>
2022-03-15 13:19:37 +0800

    feat: 添加参数判断

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # node-gridfs-toolkit
 The easy way for MongoDB Gridfs
+
+# 参数列表
+
+|名称|关键字|别名|说明|
+|-|-|-|-|
+|帮助|-h|--help|获取帮助列表|
+|主机地址|-host|无|设置数据库的主机地址|
+|端口|-port|无|设置数据库的端口|
+|用户名|-u|--user|设置数据库的用户名|
+|用户密码|-p|--password|设置数据库的用户密码|
+|认证数据库|-authenticationDatabase|无|设置数据库的用户认证数据库|
+|数据库名称|-d|--db, --database|设置所要获取的数据库的名称|
--- bin/node-gridfs-toolkit.js
+++ bin/node-gridfs-toolkit.js
@@ -1,6 +1,55 @@
 #!/usr/bin/env node
 const argv = require('optimist').argv;
+const Joi = require('joi');
+const _ = require('lodash');
 
-if (argv.h || argv.help) {
-  require('../dist/menu.js');
+function mergeArgv(argv) {
+  if (argv.port) {
+    argv.port = Number(argv.port);
+  }
+
+  if (argv.u) {
+    argv.user = argv.u;
+  }
+
+  if (argv.p) {
+    argv.password = argv.p;
+  }
+
+  if (argv.d) {
+    argv.db = argv.d;
+  }
+  return argv;
 }
+
+async function main(argv) {
+  if (argv.h || argv.help) {
+    require('../dist/menu.js');
+    return;
+  }
+
+  // TODO: 判断是否是一个参数都没传, 没传的话, 直接进入menu
+  
+  // 检查必要参数
+  const usefulArgv = _.pick(
+    mergeArgv(argv),
+    ['host', 'port', 'user', 'password', 'authenticationDatabase', 'db'],
+  );
+  const schema = Joi.object().keys({
+    host: Joi.string().optional().default('127.0.0.1'),
+    port: Joi.number().optional().default(27017),
+    user: Joi.string().optional(),
+    password: Joi.string().optional(),
+    authenticationDatabase: Joi.string().optional(),
+    db: Joi.string().required(),
+  });
+
+  try {
+    const result = await schema.validateAsync(usefulArgv);
+    console.log(result);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main(argv);
